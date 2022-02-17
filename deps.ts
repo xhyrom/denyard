9fc827ccc9abef416fc10e0bced51e3b19cce601
xhyrom <generalkubo@gmail.com>
2022-02-17 19:58:29 +0100

    fix testing

--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export { EventEmitter } from "https://deno.land/x/event@1.0.0/mod.ts";
-export { StandardWebSocketClient } from "https://deno.land/x/websocket@v0.1.3/mod.ts";
+export { EventEmitter } from 'https://deno.land/x/event@1.0.0/mod.ts';
+export { StandardWebSocketClient } from 'https://deno.land/x/websocket@v0.1.3/mod.ts';
